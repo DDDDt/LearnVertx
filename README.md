960fc48d62c09c7441cd97f98743bc50f33da91f
tao.dai <tao.dai@wehotelglobal.com>
2019-03-29 18:07:42 +0800

    eventBus

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 "# LearnVertx" 
+
+### Vert.x 启动方式
+1. 直接通过 main 方法启动
+2. 通过指令方式启动
+``
+java -jar com.Learn-Vertx-1.0.0-SNAPSHOT-fat.jar
+``
+
+参考文档: https://vertxchina.github.io/vertx-translation-chinese/start/Start.html 
+
+
--- src/main/java/com/dt/learn/vertx/MainVerticle.java
+++ src/main/java/com/dt/learn/vertx/MainVerticle.java
@@ -2,6 +2,9 @@ package com.dt.learn.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.eventbus.MessageConsumer;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -39,6 +42,59 @@ public class MainVerticle extends AbstractVerticle {
       System.out.println(i);
       req.response().end(""+i);
     }).listen(8081);
+
+    // 一次性计时器
+    vertx.setTimer(100,id -> {
+      System.out.println("一次性数据,id = "+id);
+    });
+
+    // 周期性计时器
+    long timerId = vertx.setPeriodic(1000,id -> {
+      System.out.println("周期性计时器，id = "+id);
+    });
+
+    System.out.println(timerId);
+
+    // Event Bus -> 对于每一个 Vert.x 实例来说它是单例的
+    EventBus eventBus = vertx.eventBus();
+    // 注册处理器
+    MessageConsumer<Object> consumer = eventBus.consumer("new.uk.sport", message -> {
+      System.out.println("接收到一个消息: message = " + message.body());
+    });
+
+    // 在集群模式下的 Event Bus
+    consumer.completionHandler(res -> {
+      if(res.succeeded()){
+        System.out.println("成功......");
+      }else{
+        System.out.println("失败......");
+      }
+    });
+
+    // 发布消息 -> `publish` 方法指定一个地址去发布
+    eventBus.publish("new.uk.sport","发步消息");
+
+    // 发送消息
+    eventBus.send("new.uk.sport","发送信息");
+
+    // 设置消息头 -> 通过 `DeliveryOptions` 来指定
+    DeliveryOptions options = new DeliveryOptions();
+    options.addHeader("some-header","some-value");
+    eventBus.send("new.uk.sport","带有请求头发送消息的",options);
+
+    // 应答消息和发送回复
+    MessageConsumer<Object> consumer1 = eventBus.consumer("news.uk.sport");
+    consumer1.handler(message -> {
+      System.out.println("收到一个消息并回复,message = "+message.body());
+      message.reply("回复");
+    });
+
+    // 发送
+    eventBus.send("news.uk.sport","发送信息并看有无回复",ar -> {
+      if(ar.succeeded()){
+        System.out.println("接受到回复信息: "+ar.result().body());
+      }
+    });
 
   }
 
